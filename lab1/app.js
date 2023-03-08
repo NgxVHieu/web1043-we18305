08c176d3b47d86ddf9990d1bbc4ae0eed6d695c5
NgxVHieu <127081553+NgxVHieu@users.noreply.github.com>
2023-03-08 14:19:52 +0700

    nop bai 1

---
+++ lab1/app.js
@@ -0,0 +1,24 @@
+// Nhập vào 2 số nguyên a, b từ bàn phím
+var a = parseInt(prompt("Nhập số nguyên a: "));
+var b = parseInt(prompt("Nhập số nguyên b: "));
+
+// Tính tổng c của a và b
+var c = a + b;
+
+// In ra kết quả bằng document.write
+document.write("Tổng của a và b là: " + c + "<br>");
+
+// In ra kết quả bằng console.log
+console.log("Tổng của a và b là: " + c);
+
+// Nhập vào 1 số nguyên d từ bàn phím
+var d = parseInt(prompt("Nhập số nguyên d: "));
+
+// Tính tổng result của c và d
+var result = c + d;
+
+// In ra kết quả bằng alert
+alert("Tổng của a, b và d là: " + result);
+
+// In ra kết quả bằng console.log
+console.log("Tổng của a, b và d là: " + result);
